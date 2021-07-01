df17fb707128994e7f7c51b5321fc6f054dbe2d2
VladHoron <vladyslav.horon.knm.2018@lpnu.ua>
2021-07-01 19:03:13 +0300

    Add 2

--- src/react/views/Connections/Connections.js
+++ src/react/views/Connections/Connections.js
@@ -939,7 +939,7 @@ export default class Connections extends React.Component {
 
                 {/* ------------------------------------ CONNECTION PAGE HEADER ------------------------------------ */}
                 <div className='connections-page-header'>
-                    <span className="connections-page-name">Databases</span>
+                    <span className="connections-page-name">Databases 2</span>
                     <button className="add-database-button" type="button" id="add-button"
                             onClick={() => this.openSimplifiedConnectionPopup()}>Add database
                     </button>
